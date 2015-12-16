44bb68b6c42e0f918636fc2cca79545232a31182
Jeffrey Jen <yihungjen@gmail.com>
2015-12-16 10:41:26 +0000

    DRAFT: NEW: pulling resource from origin

--- PullOrigin2S3.js
+++ PullOrigin2S3.js
@@ -1,28 +1,78 @@
 "use strict";
 
-var _ = require("lodash"),
-    ytdl = require('ytdl-core');
+var util = require("util");
+
+var debug = util.debuglog('pull-ytdl');
+
+var Promise = require("promise"),
+    request = require("request"),
+    ytdl = require("ytdl-core");
 
 var aws = require("aws-sdk-promise")(),
     s3 = aws.S3(),
     PutObject = s3.PutObject;
+
+function ytdlinfo(origin, opts) {
+    function getytinfo(ok, grr) {
+        ytdl.getInfo(origin, opts, function(err, info) {
+            if (err) {
+                grr(err);
+            } else {
+                ok(info);
+            }
+        });
+    }
+    return new Promise(getytinfo);
+}
+
+function get(url) {
+    function _get(ok, grr) {
+        request.get(url)
+            .on("response", function(res) {
+                debug("ytdl: get:", res.statusCode, res.headers);
+                if (res.statusCode === 200) {
+                    ok({body: res, contentLength: res.headers['content-length']});
+                } else {
+                    grr(res.statusCode);
+                }
+            })
+            .on("error", function(e) {
+                grr(e);
+            });
+    }
+    return new Promise(_get);
+}
 
 exports.pull = function(event, context) {
     var origin = event.Origin,
         bucket = event.Bucket,
         udat = event.UserMetaData;
 
-    var putOpts = {
-            Bucket: bucket,
-            Key: udat.miid + ":" + udat.page + ":" + udat.part + "@" + udat.src,
-            ContentType: "x-mz-custom/video",
-            StorageClass: "REDUCED_REDUNDANCY"
-        };
-
-    PutObject(_.assign(putOpts, {Body: ytdl(origin)})).then(function() {
-        context && context.success();
-    })
-    .catch(function(err) {
-        context && context.fail(err);
-    });
+    ytdlinfo(origin)
+        .then(function(info) {
+            udat.src = info.title;
+            debug("ytdl: title:", info.title);
+            debug("ytdl: url:", info.formats[0].url);
+            return get(info.formats[0].url);
+        })
+        .then(function(res) {
+            debug("putObject: size:", res.contentLength);
+            var putOpts = {
+                Bucket: bucket,
+                Key: udat.miid + ":" + udat.page + ":" + udat.part + "@" + udat.src,
+                Body: res.body,
+                ContentType: "x-mz-custom/video",
+                ContentLength: res.contentLength,
+                StorageClass: "REDUCED_REDUNDANCY"
+            };
+            return PutObject(putOpts);
+        })
+        .then(function() {
+            debug("ok");
+            context && context.success("ok");
+        })
+        .catch(function(err) {
+            debug(err);
+            context && context.fail(err);
+        });
 };
